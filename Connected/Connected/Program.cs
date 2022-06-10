e231292494083b5c5c40bb33f387a58011e91fa6
RahmadaniNurhuda <rahmadani.nurhuda@gmail.com>
2022-06-10 17:43:42 +0700

    source code

---
+++ Connected/Connected/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Connected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = GetConnectionString();
+            string query1 = "select * from Pembimbing_Akademik where BIK=333";
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd1 = new SqlCommand(query1, cn); cn.Open();
+                using (SqlDataReader dr1 = cmd1.ExecuteReader())
+                {
+                    while (dr1.Read())
+                    {
+                        string query2 = "UPDATE Pembimbing_Akademik SET Keahlian = 'Jaringan' WHERE NIK =333 ";
+                        SqlCommand cmd2 = new SqlCommand(query2, cn);
+                        cmd2.ExecuteNonQuery();
+                        Console.WriteLine("Data has been updated");
+                    }
+                }
+            }
+
+        }
+
+
+        private static string GetConnectionString()
+        {
+            return "data source=LAPTOP-I5CF3Q9T;" +
+                    "database=ProdiTI; MultipleActiveResultsSets=True; User id=sa;Password=dedekgemes123";
+        }
+    }
+}
